65ff7e53ceda6693998b9e9437ea806fe56a6fbc
tiendat171195 <tiendat.dee.95@gmail.com>
2017-03-31 22:55:20 +0700

    Fix navigator

--- index.android.js
+++ index.android.js
@@ -1,39 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 'use strict';
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Text,
-  View
+  Navigator
 } from 'react-native';
-import MapView from 'react-native-maps';
-import ScrollableTabView from 'react-native-scrollable-tab-view';
-import io from 'socket.io-client/dist/socket.io.js';
-
-import NewsFeedPage from './components/NewsFeedPage.js';
-import ChatRoomsPage from './components/ChatRoomsPage.js';
-import MapPage from './components/MapPage.js';
-import SettingPage from './components/SettingPage.js';
-
 
 import Login from './components/LogInScreen.js'
+
 export default class LocationClient extends Component {
-  
+  configureScene(route, routeStack){
+    if(route.type === 'Modal') {
+      return Navigator.SceneConfigs.FloatFromBottom
+    }
+    return Navigator.SceneConfigs.PushFromRight 
+  }
+  renderScene(route, navigator){
+    return <route.component navigator={navigator} {...route.passProps} />;
+  }
   render() {
     return (
-      
-      /*<ScrollableTabView locked={true}>
-        <NewsFeedPage tabLabel="NewsFeed" />
-        <ChatRoomsPage tabLabel="Chat Room" />
-        <MapPage tabLabel="Map" />
-        <SettingPage tabLabel="Settings" />
-      </ScrollableTabView>*/
-      <Login />
+      <Navigator
+          initialRoute={{component: Login}}
+          configureScene={this.configureScene}
+          renderScene={this.renderScene} />
+
     );
   }
 }
