ee1c602ad31fafb9e73dbf3ce102f7c4ee0a6a48
tiendat171195 <tiendat.dee.95@gmail.com>
2017-04-13 00:51:35 +0700

    Custom Map

--- components/MapPage.js
+++ components/MapPage.js
@@ -2,18 +2,218 @@
 import React, { Component } from 'react';
 import {
 	Text,
-	View
+	View,
+	Dimensions,
+	StyleSheet
 } from 'react-native';
 import MapView from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 10.74566227;
+const LONGITUDE = 106.70406818;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const customStyle = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#fff0f5',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#00ff7f',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#a52a2a',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#bdb76b',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#afeeee',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#9acd32',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#afeeee',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffdead',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#ffdead',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#ffdead',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f0e68c',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#ff7f50',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#ff7f50',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#fff0f5',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#d59563',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#00ffff',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#00ffff',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#00ffff',
+      },
+    ],
+  },
+];
+
+
+
 export default class MapPage extends Component {
 	constructor(props){
 		super(props);
 	}
 	render(){
 		return(
-			<MapView style={{flex:1}}>
-				<View style={{height:100, backgroundColor:'red'}} />
-			</MapView>
+			<View style={styles.container}>
+		        <MapView
+		          provider={this.props.provider}
+		          style={styles.map}
+		          initialRegion={{
+		            latitude: LATITUDE,
+		            longitude: LONGITUDE,
+		            latitudeDelta: LATITUDE_DELTA,
+		            longitudeDelta: LONGITUDE_DELTA,
+		          }}
+		          customMapStyle={customStyle}>
+		          	
+		          </MapView>
+		    </View>
+				
 		);
 	}
 }
+MapPage.propTypes = {
+  provider: MapView.ProviderPropType,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
