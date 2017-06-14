d34346799090f1ba6d396b14a136ff59d2caa9a0
tiendat171195 <tiendat.dee.95@gmail.com>
2017-06-14 21:55:37 +0700

    add behavior when click on icon tab bar

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -35,6 +35,19 @@ export default class MainScreen extends Component{
 				break;
 		}
 	}
+	onChangeTab(index, ref){
+		switch(index){
+			case 0:
+				Actions.chatroomspage({type: ActionConst.REFRESH});
+				break;
+			case 1:
+				Actions.map({type: ActionConst.REFRESH});
+				break;
+			case 2:
+				Actions.info({type: ActionConst.REFRESH});
+				break;
+		}
+	}
 	render(){
 		return (
 			<View style={{flex:1}}>
@@ -43,7 +56,8 @@ export default class MainScreen extends Component{
 					renderTabBar={()=><CustomTabBar />}
 					style={{backgroundColor:'sandybrown'}}
 					tabBarPosition="bottom"
-					initialPage={1}>
+					initialPage={0}
+					onChangeTab = {this.onChangeTab}>
 					<ChatRoomsPage tabLabel="ios-chatboxes" {...{'userInfo':this.props.userInfo}}/>
 					<MapPage tabLabel="md-locate" groupID={this.state._group_id} {...{'userInfo':this.props.userInfo}}/>
 					<SettingPage tabLabel="ios-settings" {...{'userInfo':this.props.userInfo}}/>
